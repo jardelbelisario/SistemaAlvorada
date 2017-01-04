812300d079e2bf85b3d752a609270fcfbefa29fa
Lucas Lima <lucaslimaandrade7@gmail.com>
2017-01-04 14:24:27 -0300

    Atualizando arquivos

--- resources/views/disciplinas/cadastro.blade.php
+++ resources/views/disciplinas/cadastro.blade.php
@@ -18,89 +18,7 @@ disciplinas
 
 
 @section('main-content')
-<<<<<<< HEAD
 
-<!-- Main content -->
-<section class="content">
-  <div class="row">
-    <div class="col-xs-12 col-md-12">
-      <!-- general form elements -->
-      
-      <div class="box box-danger">
-        <!-- form start -->
-        <form action="/home/disciplinas/cadastrar" method="POST" enctype="multipart/form-data" role="form">
-          {!! csrf_field() !!}
-          <div class="box-header with-border">
-           <!--  <h3 class="box-title">
-              <a href="/home/disciplinas/listagem" class="btn btn-default pull-left" >
-                <i class="fa fa-list"></i> Ir para listagem de disciplinas
-              </a>
-            </h3> -->
-            <h3 class="box-title">DISCIPLINAS</h3>
-            <h3 class="box-title pull-right">
-              <a href="/home/disciplinas/listagem" class="btn btn-default pull-left" >
-                <i class="fa fa-list"></i> Ir para listagem de disciplinas
-              </a>
-            </h3>
-          </div><!-- /.box-header -->
-
-          <div class="box-body">
-
-            <div class="col-xs-12 col-md-4">
-
-              <div class="col-xs-12">
-                <div class="form-group">
-                  <label for="exampleInputEmail1">Nome:</label>
-                  <input type="text" name="name" value="" placeholder="Nome" id="exampleInputNome" class="form-control">
-                </div>
-              </div>
-
-              
-
-          </div> <!-- ./col-xs-4 -->
-        </div><!-- /.box box-danger-->
-      </div>
-    </div><!-- /.row -->
-
-
-    <div class="col-xs-12">
-      <div class="box-footer">
-        <div class="form-group">
-          <button  type="submit" class="btn btn-alvorada btn-block btn-lg" type="submit">
-            Enviar Dados
-          </button>
-        </div>
-      </div>
-    </div>
-
-  </form>
-
-</div><!-- /.box primary-->
-
-</div><!-- /.col-md-12 --> 
-</div><!-- /.row -->
-
-
-<!-- Modal Chamando a tela Video -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        <video id="user-media" class="video-area tamanho-video" autoplay="autoplay" ></video>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" id="takephoto" onClick="snapshot();"  data-dismiss="modal">Salvar Foto</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-=======
 <section class="container-wrapper">
 <!-- form start -->
 <form action="/home/disciplinas/cadastrar" method="POST" enctype="multipart/form-data" role="form">
@@ -146,6 +64,6 @@ disciplinas
       </div><!-- /.col-xs-12 col-md-12-->
     </div><!-- /.row -->
  </form>
->>>>>>> bd44c5e4220ca560a7a0c8474a396e7810e5ad27
+
 </section><!-- /.content -->
 @endsection
--- resources/views/disciplinas/listagem.blade.php
+++ resources/views/disciplinas/listagem.blade.php
@@ -24,73 +24,7 @@
 
 
 @section('main-content')
-<<<<<<< HEAD
-<div class="container-wrapper">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="panel panel-default">
-        <!--<div class="panel-heading">Alunos - Pesquise por matrícula, nome, telefone, turma</div>-->
-          <div class="panel-body">
-            <div class="box box-danger">      
-              <div class="box-header with-border">
-                <h3 class="box-title">
-                  <a href="/home/disciplinas/cadastrar" class="btn btn-default pull-left" >
-                    <i class="fa fa-list"></i> Ir para cadastro de disciplinas
-                  </a>
-                </h3>
-              </div>
-              <!-- /.box-header -->
-              <div class="box-body">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                    <tr>
-                      <th>Matrícula</th>
-                      <th>Nome</th>
-                      <th>Telefone</th>
-                      <th>Turma</th>
-                      <th style="width: 100px;">Ações</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php for ($i=0; $i < 50; $i++) { ?>
-                    
-                    <tr role="row" class="odd">
-                      <td class="sorting_1">Gecko_<?= $i ?> </td>
-                      <td>Firefox 1.0s_<?= $i ?></td>
-                      <td>Win 98+ / OSX.2+_<?= $i ?></td>
-                      <td>1.7_<?= $i ?></td>
-                      <td>
-                        <div class="btn-group">
-                           <a href="/panel/pacientes/listar/pacienteFechaId" type="button" class="btn btn-primary" title="Visualizar Perfil"> <i class="fa  fa-eye"></i></a>
-                          <a href="/panel/pacientes/editar/pacienteFechaId" type="button" class="btn btn-success" title="Editar Perfil"> <i class="fa  fa-edit"></i></a>
-                          <a href="/panel/pacientes/deletar/pacienteFechaId" type="button" class="btn btn-danger" title="Deletar Perfil"> <i class="fa   fa-close"></i></a>
-                        </div>
-                      </td>
-                    </tr>
-                    
-                    <?php } ?>
-                  
-                  </tbody>
-                  <tfoot>       
-                   <tr>
-                     <th rowspan="1" colspan="1">Matrícula</th>
-                     <th rowspan="1" colspan="1">Nome</th>
-                     <th rowspan="1" colspan="1">Telefone(s)</th>
-                     <th rowspan="1" colspan="1">Turma</th>
-                     <th rowspan="1" colspan="1">Ações</th>
-                   </tr>
-                  </tfoot>
-                </table>
-               </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-=======
+
 <section class="container-wrapper">
   <div class="row">
     <div class="col-xs-12 col-md-12">
@@ -144,5 +78,5 @@
     </div>
    </div>
 </section>
->>>>>>> bd44c5e4220ca560a7a0c8474a396e7810e5ad27
+
 @endsection
